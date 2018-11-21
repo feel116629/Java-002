ad122db2c2fb75ae338bd867af219abdb39bb4df
mjw <1115272779@qq.com>
2018-11-21 15:06:01 +0800

    java-002

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;   // ±º‰ªªÀ„
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int bjt;
+		int g, s, b, q;
+		bjt = in.nextInt();
+		g = bjt % 10;
+		s = bjt % 100 / 10;
+		bjt = bjt / 100;
+		if(bjt < 8){
+			bjt = (bjt + 24 - 8);
+			System.out.print(bjt);
+			System.out.print(s);
+			System.out.println(g);
+		}
+		else if(bjt == 8)
+		{
+			if(s != 0)
+				System.out.print(s);
+			System.out.println(g);
+		}
+		else if(bjt > 8){
+			System.out.print(bjt - 8);
+			System.out.print(s);
+			System.out.println(g);
+		
+		}
+	}
+
+}
